df47a7ed649ffa2b64220447067db9019c7a6c4e
NDChien22 <nguyenduychien2206@gmail.com>
2025-12-07 00:23:20 +0700

    Update password and send mail

--- app/Livewire/Admin/Profile.php
+++ app/Livewire/Admin/Profile.php
@@ -6,6 +6,9 @@ use Livewire\Component;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use App\Helpers\Cmail;
 
 class Profile extends Component
 {
@@ -14,6 +17,8 @@ class Profile extends Component
     protected $queryString = ['tab'=>['keep'=>true]];
 
     public $name, $email, $username, $bio;
+
+    public $current_password, $new_password, $new_password_confirmation;
 
     protected $listeners = [
         'updateProfile' => '$refresh'
@@ -55,6 +60,55 @@ class Profile extends Component
             $this->dispatch('updateTopUserInfo')->to(TopUserInfo::class);
         }else{
             $this->dispatch('showToastr', ['type' => 'error', 'message'=>'Somthing went wrong.']);
+        }
+    }
+
+    public function updatePassword(){
+        $user = User::findOrFail(Auth::id());
+
+        //validate
+        $this->validate([
+            'current_password' => [
+                'required',
+                'min: 5',
+                function($attribute, $value, $fail) use ($user) {
+                    if(!Hash::check($value, $user->password)){
+                        return $fail(__('Your current password does not match our records.'));
+                    }
+                }
+            ],
+            'new_password' => 'required|min:5|confirmed',
+        ]);
+
+        //Update Password
+        $update = $user->update([
+            'password' => Hash::make(($this->new_password))
+        ]);
+
+        if($update){
+            //Send Email Notification
+            $data = array(
+                'user' =>$user,
+                'new_password' => $this->new_password,
+            );
+
+            $mail_body = view('email-templates.password-changes-template', $data)->render();
+
+            $mail_cofig = array(
+                'recipient_address' => $user->email,
+                'recipient_name' => $user->name,
+                'subject' => 'Password Changed',
+                'body' => $mail_body,
+            );
+
+            Cmail::send($mail_cofig);
+
+            //Logout and Redirect to login
+            Auth::logout();
+            Session::flash('info', 'Your password has been changed successfully. Please login with your new password.');
+            $this->redirectRoute('admin.login');
+        }else{
+            $this->dispatch('showToastr', ['type'=>'error', 'message'=>'Somthing went wrong.']);
         }
     }
 
--- resources/views/livewire/admin/profile.blade.php
+++ resources/views/livewire/admin/profile.blade.php
@@ -7,7 +7,7 @@
                         onclick="event.preventDefault();document.getElementById('profilePictureFile').click();"
                         class="edit-avatar"><i class="fa fa-pencil"></i></a>
                     <div>
-                        <img src="{{ $user->picture }}" alt="" class="avatar-photo" id="profilePicturePreview" >
+                        <img src="{{ $user->picture }}" alt="" class="avatar-photo" id="profilePicturePreview">
                     </div>
                     <input type="file" name="profilePictureFile" id="profilePictureFile" class="d-none"
                         style="opacity: 0">
@@ -152,7 +152,43 @@
                             <div class="tab-pane fade {{ $tab == 'update_password' ? 'show active' : '' }}"
                                 id="update_password" role="tabpanel">
                                 <div class="pd-20 profile-task-wrap">
-                                    ---- Update Password ----
+                                    <form wire:submit="updatePassword()">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <label for="">Current Password</label>
+                                                    <input type="password" class="form-control"
+                                                        wire:model="current_password"
+                                                        placeholder="Enter current password">
+                                                    @error('current_password')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <label for="">New Password</label>
+                                                    <input type="password" class="form-control"
+                                                        wire:model="new_password" placeholder="Enter new password">
+                                                    @error('new_password')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <label for="">Confirm new Password</label>
+                                                    <input type="password" class="form-control"
+                                                        wire:model="new_password_confirmation"
+                                                        placeholder="Confirm new password">
+                                                    @error('new_password_confirmation')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Update Password</button>
+                                    </form>
                                 </div>
                             </div>
 
