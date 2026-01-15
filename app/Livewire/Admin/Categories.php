aaaa2de4bfd0cf48f8b8260c68d3d18b23218434
NDChien22 <nguyenduychien2206@gmail.com>
2026-01-15 21:38:10 +0700

    Add parent category

---
+++ app/Livewire/Admin/Categories.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+use App\Models\ParentCategory;
+use App\Models\Category;
+
+class Categories extends Component
+{
+    public $isUpdateParentCategoryMode = false;
+    public $pcategory_id, $pcategory_name;
+
+    public function addParentCategory()
+    {
+        $this->pcategory_id = null;
+        $this->pcategory_name = null;
+        $this->isUpdateParentCategoryMode = false;
+        $this->showParentCategoryModalForm();
+    }
+
+    public function createParentCategory()
+    {
+        $this->validate([
+            'pcategory_name' => 'required|unique:parent_categories,name',
+        ],[
+            'pcategory_name.required' => 'Parent category field is required.',
+            'pcategory_name.unique' => 'Parent category name already exists.',
+        ]);
+
+        //Store new parent category
+        $pcategory = new ParentCategory();
+        $pcategory->name = $this->pcategory_name;
+        $saved = $pcategory->save();
+
+        if($saved){
+            $this->hideParentCategoryModalForm();
+            $this->dispatch('showToastr', ['type' => 'success', 'message' => 'Parent category created successfully.']);
+        }else{
+            $this->dispatch('showToastr', ['type' => 'error', 'message' => 'Failed to create parent category.']);
+        }
+    }
+
+    public function showParentCategoryModalForm(){
+        $this->resetErrorBag();
+        $this->dispatch('showParentCategoryModalForm');
+    }
+
+    public function hideParentCategoryModalForm(){
+        $this->dispatch('hideParentCategoryModalForm');
+        $this->isUpdateParentCategoryMode = false;
+        $this->pcategory_id = $this->pcategory_name = null;
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.categories');
+    }
+}
---
+++ resources/views/livewire/admin/categories.blade.php
@@ -0,0 +1,124 @@
+<div>
+    <div class="row">
+        <div class="col-12">
+            <div class="pd-20 card-box mb-30">
+                <div class="clearfix">
+                    <div class="pull-left">
+                        <div class="h4 text-blue">Parent categories</div>
+                    </div>
+                    <div class="pull-right">
+                        <a href="javascript:; " wire:click="addParentCategory()" class="btn btn-primary btn-sm">Add P.
+                            category</a>
+                    </div>
+                </div>
+                <div class="table-responsive mt-4">
+                    <table class="table table-borderless table-striped table-sm">
+                        <thead class="bg-secondary text-white">
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>N. of categories</th>
+                            <th>Actions</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>P. Cat 1</td>
+                                <td>4</td>
+                                <td>
+                                    <div class="table-actions">
+                                        <a href="" class="text-primary mx-2">
+                                            <i class="dw dw-edit2"></i>
+                                        </a>
+                                        <a href="" class="text-danger mx-2">
+                                            <i class="dw dw-delete-3"></i>
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12">
+            <div class="pd-20 card-box mb-30">
+                <div class="clearfix">
+                    <div class="pull-left">
+                        <div class="h4 text-blue">Categories</div>
+                    </div>
+                    <div class="pull-right">
+                        <a href="" class="btn btn-primary btn-sm">Add category</a>
+                    </div>
+                </div>
+                <div class="table-responsive mt-4">
+                    <table class="table table-borderless table-striped table-sm">
+                        <thead class="bg-secondary text-white">
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Parent category</th>
+                            <th>N. of posts</th>
+                            <th>Actions</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>P. Cat 1</td>
+                                <td>Any</td>
+                                <td>4</td>
+                                <td>
+                                    <div class="table-actions">
+                                        <a href="" class="text-primary mx-2">
+                                            <i class="dw dw-edit2"></i>
+                                        </a>
+                                        <a href="" class="text-danger mx-2">
+                                            <i class="dw dw-delete-3"></i>
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    //Modal for adding/editing parent category
+
+    <div wire:ignore.self class="modal fade" id="pcategory_modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+        aria-hidden="true" data-backdrop="static" data-keyboard="false">
+        <div class="modal-dialog modal-dialog-centered">
+            <form class="modal-content" wire:submit="{{$isUpdateParentCategoryMode ? 'updateParentCategory()' : 'createParentCategory()' }}">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="myLargeModalLabel">
+                        {{ $isUpdateParentCategoryMode ? 'Update P. Category' : 'Add P. Category' }}
+                    </h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+                        ×
+                    </button>
+                </div>
+                <div class="modal-body">
+                    @if ($isUpdateParentCategoryMode)
+                    <input type="hidden" wire:model="pcategory_id">
+                    @endif 
+                    <div class="form-group">
+                        <label for=""><b>Parent Category Name</b></label>
+                        <input type="text" class="form-control" wire:model="pcategory_name" placeholder="Enter parent category name">
+                        @error('pcategory_name')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div> 
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                        Close
+                    </button>
+                    <button type="submit" class="btn btn-primary">
+                        {{$isUpdateParentCategoryMode ? 'Save changes' : 'Create' }}
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
